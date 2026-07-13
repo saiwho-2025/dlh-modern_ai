a3b7894891ff164474a157bdd1e9e9f1a217fb7e
saihu <laura.sai.hu@gmail.com>
2026-07-14 00:32:51 +0200

    f-string? 3-character difference

--- data_analysis/data_preparation_visualization/0-describe_data.py
+++ data_analysis/data_preparation_visualization/0-describe_data.py
@@ -4,10 +4,11 @@
 import pandas as pd
 
 df = pd.read_csv('Telco-Customer-Churn.csv')
-shape = df.shape
-data_types = df.dtypes
-head = df.head()
-missing_count = df.isna().sum()
-duplicates = df.duplicated().sum()
-print(f"Shape: {shape}\nDtypes:\n{data_types}\nFirst rows:\n{head}")
-print(f"Missing values:\n{missing_count}\nDuplicates: {duplicates}")
+print("Shape:", df.shape)
+print("Dtypes:")
+print(df.dtypes)
+print("First rows:")
+print(df.head())
+print("Missing values:")
+print(df.isna().sum())
+print("Duplicates:", df.duplicated().sum())
