11d35466259871033b101fcf32731e54bd3f29c3
saihu <laura.sai.hu@gmail.com>
2026-07-15 10:05:57 +0200

    creation

---
+++ data_analysis/data_preparation_visualization/5-drop_customerID.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""the function removes customerID column """
+
+
+def drop_costomerID(df):
+    """the function removes the column and returns updated dp"""
+    cleaned_df = df.drop(columns = ["customerID"])
+    return cleaned_df
