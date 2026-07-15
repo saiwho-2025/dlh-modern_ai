a67ea387ef98bbfb088b14458a610cf194a73457
saihu <laura.sai.hu@gmail.com>
2026-07-15 08:57:08 +0200

    fix

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""a function handles missing values"""
+"""a function that cleans data of NA, set"""
 
 
 def clean_total_charges(df, method='drop'):
-    """try put drop in the last"""
+    """ normalize and drop"""
     def to_float(value):
+        """value type float"""
         try:
             return float(value)
         except (ValueError, TypeError):
