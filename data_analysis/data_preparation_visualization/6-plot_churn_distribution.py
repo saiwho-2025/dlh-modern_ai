35b9bc68bf421442745a2f93948a8b754f68c430
saihu <laura.sai.hu@gmail.com>
2026-07-15 16:32:05 +0200

    plot

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """a function visualize churn class distribution"""
+import matplotlib.pyplot as plt
 
 
 def plot_churn_distribution(df):
-    """the plot"""
-    counts = df["Churn"].value_counts()
-    counts.plot(kind="bar", color=["skyblue", "salmon"])
+    """Visualize churn class distribution."""
+    churn_counts = df["Churn"].value_counts()
+    colors = [
+        "skyblue" if value == "No" else "salmon"
+        for value in churn_counts.index
+    ]
+
+    plt.bar(churn_counts.index, churn_counts.values, color=colors)
+    plt.xlabel("Churn")
+    plt.ylabel("Count")
+    plt.title("Churn Distribution")
+    plt.show()
