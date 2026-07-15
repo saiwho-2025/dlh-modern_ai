a207e7940b950ef16f6639896d855e9a91d7e8cd
saihu <laura.sai.hu@gmail.com>
2026-07-15 08:14:55 +0200

    try

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -4,28 +4,3 @@
 
 def clean_total_charges(df, method='drop'):
     """the method handles different missing values in PandsDataFrame"""
-    def to_numeric(value):
-        try:
-            return float(value)
-        except (ValueError, TypeError):
-            return float("nan")
-
-    df["TotalCharges"] = df["TotalCharges"].apply(to_numeric)
-
-    if method == "drop":
-        df = df.dropna(subset=["TotalCharges"])
-
-    elif method == "median":
-        df["TotalCharges"] = df["TotalCharges"].fillna(
-            df["TotalCharges"].median()
-        )
-
-    elif method == "impute":
-        df["TotalCharges"] = df["TotalCharges"].fillna(
-            df["MonthlyCharges"] * df["tenure"]
-        )
-
-    else:
-        raise ValueError("method must be 'drop', 'median', or 'impute'")
-
-    return df
