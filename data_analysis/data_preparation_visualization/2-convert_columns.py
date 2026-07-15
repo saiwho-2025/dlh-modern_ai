50bffb8e676c262ac42f0a119771f7c013d7a8a8
saihu <laura.sai.hu@gmail.com>
2026-07-15 07:28:56 +0200

    update

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def convert_columns(df):
+    """change columns as required"""
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
     df["SeniorCitizen"] = df["SeniorCitizen"].map({0: "No", 1: "Yes"})
     return df
