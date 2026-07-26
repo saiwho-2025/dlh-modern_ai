59ee3b85b74758ec6080958b9e2c94d714ad6d53
saihu <laura.sai.hu@gmail.com>
2026-07-26 14:40:11 +0200

    typo

---
+++ machine_learning/linear_models/0-linear_regression.py
@@ -0,0 +1,7 @@
+from sklearn import linear_model
+
+
+def Linear_Regression():
+    """Return an untrained ordinary least squares linear regression model."""
+    model = linear_model.LinearRegression()
+    return model
