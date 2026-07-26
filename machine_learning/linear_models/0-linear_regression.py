44e45cc471cdee536c555ff8d7e4fe4e7b7ec19c
saihu <laura.sai.hu@gmail.com>
2026-07-26 20:17:00 +0200

    update

--- machine_learning/linear_models/0-linear_regression.py
+++ machine_learning/linear_models/0-linear_regression.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-    """Return an untrained ordinary least squares linear regression model."""
+"""Module that creates an ordinary least squares linear regression model."""
+
 from sklearn import linear_model
 
 
 def Linear_Regression():
-    """Return an untrained ordinary least squares linear regression model."""
-    model = linear_model.LinearRegression()
-    return model
+    """Return an untrained LinearRegression model."""
+    return linear_model.LinearRegression()
