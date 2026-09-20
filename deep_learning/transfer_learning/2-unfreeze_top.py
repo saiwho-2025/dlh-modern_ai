f3f3725b2921df9c980f3b2667b9fe7f2918583b
saihu <laura.sai.hu@gmail.com>
2026-09-20 21:10:42 +0200

    fix

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -4,14 +4,11 @@
 
 def unfreeze_top_layers(model, n_layers):
     """Unfreeze the last n layers of the model."""
-    # Make the base model trainable.
-    model.trainable = True
-
-    # Freeze all layers first.
+    # Freeze all layers in the base model.
     for layer in model.layers:
         layer.trainable = False
 
-    # Unfreeze only the last requested layers.
+    # Unfreeze only the requested last layers.
     if n_layers > 0:
         for layer in model.layers[-n_layers:]:
             layer.trainable = True
